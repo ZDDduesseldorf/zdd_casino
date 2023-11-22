ae4ffbf671ce2dc20cbfe5a093b6b47f306589da
Florian Huber <florian.huber@hs-duesseldorf.de>
2023-11-22 13:38:08 +0100

    add test for play_game

--- tests/test_main_functions.py
+++ tests/test_main_functions.py
@@ -1,5 +1,8 @@
 import pytest
 from zdd_casino.main_functions import count_cards
+from zdd_casino.main_functions import play_game
+from zdd_casino.main_classes import CardDeck
+from io import StringIO
 
 
 def test_count_cards_no_aces():
@@ -14,3 +17,34 @@ def test_count_cards_no_aces():
                           ])
 def test_count_cards_with_aces(cards, points):
     assert count_cards(cards) == points
+
+
+def test_single_player_blackjack(monkeypatch):
+    """Simulate a single player getting Blackjack."""
+    def mock_user_input():
+        # This simulates the user input
+        def fake_input(prompt):
+            if "number of players" in prompt:
+                return "1"
+            return "n"
+        return fake_input
+    
+    def mock_draw_cards(self, n):
+        # This simulates the drawing of cards.
+        return ["A", "10"]
+
+    # Mock single player game
+    monkeypatch.setattr('builtins.input', mock_user_input())
+
+    # Mock CardDeck to deal a Blackjack hand
+    monkeypatch.setattr(CardDeck, 'draw_cards', mock_draw_cards)
+
+    # Capture output
+    output = StringIO()
+    monkeypatch.setattr('sys.stdout', output)
+
+    play_game()
+
+    assert "Player 1, your cards: A, 10" in output.getvalue()
+    assert "You have 21 points." in output.getvalue()
+    assert "Winner(s): Player 1" in output.getvalue()
